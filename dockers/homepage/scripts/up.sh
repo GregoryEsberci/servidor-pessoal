a9238aaf6d31f66f506e76fe99c5f630a3fcc8af
GregoryEsberci <gregorym.esberci@gmail.com>
2025-06-11 09:53:31 -0300

    configurado nginx

--- dockers/homepage/scripts/up.sh
+++ dockers/homepage/scripts/up.sh
@@ -2,6 +2,5 @@
 
 export TZ=$(timedatectl show --property=Timezone --value)
 export SERVICE_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
-export HOMEPAGE_ALLOWED_HOSTS="$(curl -s ifconfig.me),$(hostname -I | sed -r "s/ /,/g")"
 
 docker compose -f "$SERVICE_DIR/docker-compose.yml" up -d
